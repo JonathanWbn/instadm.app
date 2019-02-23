0a2489e3b1a7f6ea6af04cdf397bb8bd99013dfc
Jonathan Wieben <joni.wieben@icloud.com>
2019-02-23 13:42:31 +0100

    Chore: Scaffold login form and api login

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,37 @@
-export default () => <div>YOYOYO</div>
+import { useState } from 'react'
+
+function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () => {
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
+  return (
+    <>
+      <input
+        value={username}
+        placeholder="username"
+        onChange={e => setUsername(e.target.value)}
+      />
+      <input
+        value={password}
+        placeholder="password"
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </>
+  )
+}
+
+export default App
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+var Client = require('instagram-private-api').V1
+var bodyParser = require('body-parser')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,10 +9,45 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express().use(bodyParser.json())
 
   server.get('*', (req, res) => {
     return handle(req, res)
+  })
+
+  server.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const device = new Client.Device(username)
+    const storage = new Client.CookieMemoryStorage()
+
+    const followDanBilzerian = async () => {
+      try {
+        const session = await Client.Session.create(device, storage, username, password)
+        const account = await Client.Account.searchForUser(session, 'danbilzerian')
+        const relation = await Client.Relationship.create(session, account.id)
+        console.log('successfully subscribed to dan bilzerian')
+        return relation
+      } catch (error) {
+        if (error.json && error.json.error_type === 'checkpoint_challenge_required') {
+          console.log('solving challenge')
+          try {
+            const challenge = await Client.Web.Challenge.resolve(error, 'phone')
+            if (challenge.type !== 'phone') return
+            await challenge.phone()
+            await challenge.code('037825')
+          } catch (challengeError) {
+            console.log('challenge failed', challengeError.message)
+          }
+          console.log('challenge succeeded. logging in again')
+          await followDanBilzerian()
+        } else {
+          // TODO: Login required to process this request
+          console.log('error', error.message)
+        }
+      }
+    }
+
+    await followDanBilzerian()
   })
 
   server.listen(port, err => {
