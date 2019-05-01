038776c990ad46a294851f33f768775ef2542b5b
Jonathan Wieben <joni.wieben@icloud.com>
2019-05-01 15:45:56 +0200

    WIP: Add basic authentication

---
+++ pages/login.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <form v-on:submit.prevent="onSubmit(username, password);">
+      <h1 class="title">Instagram DM web app</h1>
+      <input type="text" name="username" class="input" v-model="username">
+      <input type="password" name="password" class="input" v-model="password">
+      <button type="submit">submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      password: null
+    };
+  },
+  methods: {
+    onSubmit(email, password) {
+      window.fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username: email, password })
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
--- server/index.js
+++ server/index.js
@@ -1,34 +1,66 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const express = require("express");
+const consola = require("consola");
+const { Nuxt, Builder } = require("nuxt");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local").Strategy;
 
-// Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
+const config = require("../nuxt.config.js");
 
-async function start() {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+config.dev = !(process.env.NODE_ENV === "production");
 
-  const { host, port } = nuxt.options.server
+const app = express();
 
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    return done(null, { username, password });
   })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  done(err, { username });
+});
+
+const nuxt = new Nuxt(config);
+
+const { host, port } = nuxt.options.server;
+
+if (config.dev) {
+  const builder = new Builder(nuxt);
+  builder.build();
+} else {
+  nuxt.ready();
 }
-start()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
+
+app.get("/", (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.status(301).redirect("/login");
+  }
+});
+
+app.use(nuxt.render);
+
+// Listen the server
+app.listen(port, host);
+consola.ready({
+  message: `Server listening on http://${host}:${port}`,
+  badge: true
+});
